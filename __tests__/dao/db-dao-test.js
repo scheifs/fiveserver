2c2c0bc7c1e779bc3f441fe3cd41ed468f1276d5
Jeremy Scheifele <scheifs@gmail.com>
2020-06-10 20:47:33 -0500

    db dao unit test

--- __tests__/dao/db-dao-test.js
+++ __tests__/dao/db-dao-test.js
@@ -28,6 +28,22 @@ describe('dbdao connect', () => {
 
 });
 
+describe('dbdao disconnect', () => {
+
+  test('client calls logout on disconnect', async () => {
+
+    const clientMock = {
+      logout: jest.fn()
+    };
+    const dbdao = new DBDao(clientMock);
+
+    await dbdao.disconnect();
+    expect(clientMock.logout).toHaveBeenCalled();
+
+  });
+
+});
+
 describe('dbdao insert', () => {
 
   test('promise resolves when insert is successful', async () => {
@@ -41,9 +57,6 @@ describe('dbdao insert', () => {
         }
       })
     };
-
-
-    console.log(dbCollectionMock.insertOne('doc'));
 
     const dbMock = {
       collection: jest.fn(() => dbCollectionMock)
@@ -66,15 +79,156 @@ describe('dbdao insert', () => {
 
     const clientMock = {
       connect: jest.fn((cb) => cb(clientMock)),
-      db: jest.fn(() => { throw err})
+      db: jest.fn(() => { throw err })
     }
 
     const dbdao = new DBDao(clientMock);
-    
+
     await expect(dbdao.insert('db', 'collection', {})).rejects.toEqual('error');
 
 
   });
+
+});
+
+describe('findOneWithSearchCriteria', () => {
+
+  let dbCollectionMock;
+  let databaseMock;
+  let clientMock;
+  let dbdao;
+
+  beforeEach(() => {
+
+    dbCollectionMock = jest.fn();
+
+    databaseMock = {
+      collection: jest.fn(() => dbCollectionMock)
+    }
+
+    clientMock = {
+      db: jest.fn(() => databaseMock)
+    }
+
+    dbdao = new DBDao(clientMock);
+  });
+
+  test('promise resolves with search result', async () => {
+
+    dbCollectionMock = {
+      findOne: jest.fn(() => { return "foundOne"})
+    };  
+    
+    await expect(dbdao.findOneWithSearchCriteria(databaseMock, "collection", {})).resolves.toBe('foundOne');
+
+  });
+
+  test('promise rejects with error on findOne failure', async () => {
+
+    dbCollectionMock = {
+      findOne: jest.fn(() => { throw "found error"})
+    };  
+    
+    await expect(dbdao.findOneWithSearchCriteria(databaseMock, "collection", {})).rejects.toBe('found error');
+
+  });
+
+
+});
+
+describe('findWithSearchCriteria', () => {
+
+  let dbCollectionMock;
+  let databaseMock;
+  let clientMock;
+  let dbdao;
+
+  beforeEach(() => {
+
+    dbCollectionMock = jest.fn();
+
+    databaseMock = {
+      collection: jest.fn(() => dbCollectionMock)
+    }
+
+    clientMock = {
+      db: jest.fn(() => databaseMock)
+    }
+
+    dbdao = new DBDao(clientMock);
+  });
+
+  test('promise resolves with search result', async () => {
+
+    const resp = "[a,b]";
+
+    const dbResponse = {
+      toArray: jest.fn(() => resp)
+    }
+
+    dbCollectionMock = {
+      find: jest.fn(() => dbResponse)
+    };  
+    
+    await expect(dbdao.findWithSearchCriteria(databaseMock, "collection", {})).resolves.toBe(resp);
+
+  });
+
+  test('promise rejects with error on search failure', async () => {
+
+    dbCollectionMock = {
+      find: jest.fn(() => { throw "find error"})
+    };   
+    
+    await expect(dbdao.findWithSearchCriteria(databaseMock, "collection", {})).rejects.toBe('find error');
+
+  });
+
+
+});
+
+describe('deleteMany', () => {
+
+  let dbCollectionMock;
+  let databaseMock;
+  let clientMock;
+  let dbdao;
+
+  beforeEach(() => {
+
+    dbCollectionMock = jest.fn();
+
+    databaseMock = {
+      collection: jest.fn(() => dbCollectionMock)
+    }
+
+    clientMock = {
+      db: jest.fn(() => databaseMock)
+    }
+
+    dbdao = new DBDao(clientMock);
+  });
+
+  test('promise resolves with delete many result', async () => {
+
+    dbCollectionMock = {
+      deleteMany: jest.fn(() => { return "deleted"})
+    };  
+    
+    await expect(dbdao.deleteMany(databaseMock, "collection", {})).resolves.toBe('deleted');
+
+  });
+
+  test('promise rejects with error on delete failure', async () => {
+
+    dbCollectionMock = {
+      deleteMany: jest.fn(() => { throw "delete error"})
+    };  
+    
+    await expect(dbdao.deleteMany(databaseMock, "collection", {})).rejects.toBe('delete error');
+
+  });
+
 
 });
 
